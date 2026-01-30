84f6b8e103c8188fa2dfdb890408ee7c3fb78ef2
VISHWAJ33T <vishwajeety14122@gmail.com>
2026-01-31 04:34:32 +0530

    refactor: remove workflow orchestration export to streamline module exports

--- packages/ai-router/src/index.ts
+++ packages/ai-router/src/index.ts
@@ -2,5 +2,3 @@ export * from './router.js';
 export * from './types.js';
 export * from './helper.js';
 export * from './store.js';
-// export * from './workflow.js';
-export * from './workflow/orchestrate.js';
